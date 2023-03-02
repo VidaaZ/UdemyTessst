e2edb1557424c46be11870c3249442add2192336
Vida <116600986+VidaaZ@users.noreply.github.com>
2023-03-02 14:53:16 -0500

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UdemyWeb.Model;
+
+namespace UdemyWeb.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+
+        }
+        public DbSet<Category> Category { get; set; }
+    }
+}
